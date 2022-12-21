1ed04459a696ffcf3c29eff4ccb14548e1b17a44
aymanb <aymanb@users.noreply.github.com>
2022-12-20 21:27:50 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,11 @@
+import numpy as np
+import plotly.graph_objects as go
 import streamlit as st
 
-st.write('Hello world!')
+z = np.random.random_sample((3, 2))
+fig = go.Figure(data=go.Contour(z=z))
+
+st.plotly_chart(
+    fig, 
+    theme="streamlit",  # ✨ Optional, this is already set by default!
+)
